a6b63b037eac7d24138bac6f9643f3ea9780f936
ruirun <indevruis@gmail.com>
2024-02-03 11:13:10 +0900

    Feat: SettingScreen UI

---
+++ lib/models/setting.dart
@@ -0,0 +1,109 @@
+class Setting {
+  final String title;
+  final List<Menu> menus;
+
+  const Setting({
+    required this.title,
+    required this.menus,
+  });
+}
+
+class Menu {
+  final String menu;
+  final Function() onPress;
+
+  Menu({
+    required this.menu,
+    required this.onPress,
+  });
+}
+
+List<Setting> settings = [
+  Setting(
+    title: "일반",
+    menus: [
+      Menu(
+        menu: "내 정보",
+        onPress: () {
+          '내 정보';
+        },
+      ),
+      Menu(
+        menu: "공지사항",
+        onPress: () {
+          '공지사항';
+        },
+      ),
+      Menu(
+        menu: "로그아웃",
+        onPress: () {
+          '로그아웃';
+        },
+      ),
+      Menu(
+        menu: "회원탈퇴",
+        onPress: () {
+          '회원탈퇴';
+        },
+      ),
+    ],
+  ),
+  Setting(
+    title: "내 활동",
+    menus: [
+      Menu(
+        menu: "내가 쓴 글",
+        onPress: () {
+          '내가 쓴 글';
+        },
+      ),
+      Menu(
+        menu: "내가 쓴 댓글",
+        onPress: () {
+          '내가 쓴 댓글';
+        },
+      ),
+      Menu(
+        menu: "좋아요 누른 글",
+        onPress: () {
+          '좋아요 누른 글';
+        },
+      ),
+      Menu(
+        menu: "리트윗한 글",
+        onPress: () {
+          '리트윗한 글';
+        },
+      ),
+    ],
+  ),
+  Setting(
+    title: "문의 및 알림",
+    menus: [
+      Menu(
+        menu: "알림 설정",
+        onPress: () {
+          '알림 설정';
+        },
+      ),
+      Menu(
+        menu: "고객센터",
+        onPress: () {
+          '고객센터';
+        },
+      ),
+      Menu(
+        menu: "약관 및 정책",
+        onPress: () {
+          '약관 및 정책';
+        },
+      ),
+      Menu(
+        menu: "현재 버전 0.0.1",
+        onPress: () {
+          '현재 버전 0.0.1';
+        },
+      ),
+    ],
+  ),
+];
--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:recycle/models/follow.dart';
+import 'package:recycle/models/setting.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
@@ -10,6 +13,141 @@ class SettingScreen extends StatefulWidget {
 class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: renderAppBar(),
+      body: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            userInfWidget(),
+            menusWidget(),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  AppBar renderAppBar() => AppBar(
+        backgroundColor: Colors.white,
+        bottomOpacity: 0,
+        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications_none, color: Colors.black),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+      );
+
+  Widget userInfWidget() => Container(
+        margin: const EdgeInsets.only(bottom: 20.0),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(100.0),
+              child: Image.asset(
+                'assets/images/profile.jpg',
+                width: 80.0,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '이름',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w500, fontSize: 20.0),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Row(
+                      children: follows
+                          .map((follow) => Row(
+                                children: [
+                                  Icon(
+                                    follow.icon,
+                                    size: 20.0,
+                                    color: Colors.grey,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 4.0),
+                                    child: Text(follow.type),
+                                  ),
+                                  Text(follow.count.toString()),
+                                  const SizedBox(width: 10.0),
+                                ],
+                              ))
+                          .toList(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget menusWidget() => Column(
+      children: settings
+          .map((menu) => Column(
+                children: [
+                  titleWidget(title: menu.title),
+                  ...menu.menus
+                      .map((menu) => menuWidget(
+                            menu: menu.menu,
+                            onPress: () {},
+                          ))
+                      .toList()
+                ],
+              ))
+          .toList());
+
+  Widget titleWidget({required String title}) => Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                border: Border(
+                    bottom: BorderSide(color: Colors.grey.withOpacity(0.5)))),
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Text(
+                title,
+                style:
+                    const TextStyle(color: Color(0xff008000), fontSize: 17.0),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+        ],
+      );
+
+  Widget menuWidget({required String menu, required Function onPress}) {
+    return SizedBox(
+        width: double.infinity,
+        child: TextButton(
+          onPressed: () {},
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                menu,
+                style: TextStyle(fontSize: 14.0, color: Colors.grey[600]),
+              ),
+              const Icon(Icons.arrow_forward_ios_rounded,
+                  color: Colors.grey, size: 16.0),
+            ],
+          ),
+        ));
   }
 }
