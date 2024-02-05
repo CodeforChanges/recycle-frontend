17bd52e9dc2088576cb8ba4abfcd06ae2e1583ca
ruirun <indevruis@gmail.com>
2024-02-05 14:06:12 +0900

    Chore: add GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 import 'package:recycle/routes.dart';
 import 'package:camera/camera.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       initialRoute: '/',
       theme: ThemeData(
         primarySwatch: Colors.grey,
