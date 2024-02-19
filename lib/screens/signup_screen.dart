5d41bd5fc3056d2f95cf044729a6affb9ecec98b
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-02-19 16:36:02 +0900

    fix: images 폴더 수정 후 pubspec 업데이트

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/route_manager.dart';
 import 'package:recycle/controller/auth_service.dart';
 import 'package:recycle/models/user.dart';
 
